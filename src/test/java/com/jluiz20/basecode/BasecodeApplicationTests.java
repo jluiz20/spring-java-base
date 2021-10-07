561a24978eaf74384024a4078d11fa34de1242c8
Joao Luiz Vieira <vieira.jluiz@gmail.com>
2021-10-07 16:29:09 -0300

    first commit

---
+++ src/test/java/com/jluiz20/basecode/BasecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jluiz20.basecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
